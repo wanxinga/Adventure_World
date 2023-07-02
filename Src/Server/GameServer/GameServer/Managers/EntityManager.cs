ee43b28ee98989fad50c1d1e304e37cf32556070
wanxing <564534235@qq.com>
2023-07-02 12:57:43 +0800

    1

---
+++ Src/Server/GameServer/GameServer/Managers/EntityManager.cs
@@ -0,0 +1,33 @@
+﻿using Common;
+using GameServer.Entities;
+using System.Collections.Generic;
+
+namespace GameServer.Managers
+{
+    class EntityManager : Singleton<EntityManager>
+    {
+        private int idx = 0;
+        public List<Entity> AllEntities = new List<Entity>();
+        public Dictionary<int, List<Entity>> MapEntities = new Dictionary<int, List<Entity>>();
+
+        public void AddEntity(int mapId, Entity entity)
+        {
+            AllEntities.Add(entity);
+            //加入管理器生成唯一ID
+            entity.EntityData.Id = ++this.idx;
+            List<Entity> entities = null;
+            if (!MapEntities.TryGetValue(mapId, out entities))
+            {
+                entities = new List<Entity>();
+                MapEntities[mapId] = entities;
+            }
+            entities.Add(entity);
+        }
+
+        public void RemoveEntity(int mapId, Entity entity)
+        {
+            this.AllEntities.Remove(entity);
+            this.MapEntities[mapId].Remove(entity);
+        }
+    }
+}
